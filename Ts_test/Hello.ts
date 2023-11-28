01bb7ddf6f8603861d44d6ecf683d80886577048
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2023-11-28 12:01:44 +0900

    add Hello

--- Ts_test/Hello.ts
+++ Ts_test/Hello.ts
@@ -1,17 +1,24 @@
+// TypeInference で string型が自動的に設定
 var message = "Hello!!!!";
-
-console.log(message);
-
+// string型に number型は入れられない
+// message = 4;
 if (message) {
     console.log(message);
 }
 
+// Type Inference （型推論）
 var id = 1;
-var email:string = "test@gmail.com";
+var email = "test@test.com";
+var isActive = true;
+var none;
+
+// Type Definition （型定義）
+var id:number = 1;
+var email:string = "test@test.com";
 var isActive:boolean = true;
 var none:any;
 
-// none = "D";
+// none = "YSE";
 // none = 5;
 
 console.log(id, typeof id);
