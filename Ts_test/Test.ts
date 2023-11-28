c5ab8b509c06dee4e7ff05713f832e160fda24e6
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2023-11-28 12:04:24 +0900

    add test

---
+++ Ts_test/Test.ts
@@ -0,0 +1,22 @@
+var id:number = 1;
+var email:string = "test@test.com";
+var isActive:boolean = true;
+var none:any;
+
+console.log(typeof id)
+
+var id:number = 1;
+var email:string = "test@test.com";
+var isActive:boolean = true;
+var none:any;
+
+console.log(typeof isActive)
+
+
+var id:number = 1;
+var email:string = "test@test.com";
+var isActive:boolean = true;
+var none:any;
+// var email = 120;
+var email = "120";
+console.log(typeof email)
