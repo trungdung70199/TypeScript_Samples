713b90174598d113e87a7bfba8e2924a6a2e9453
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2023-11-28 11:03:49 +0900

    add hello

--- Ts_test/Hello.ts
+++ Ts_test/Hello.ts
@@ -1,2 +1,16 @@
 var message = "Hello!!!!";
+
 console.log(message);
+
+if (message) {
+    console.log(message);
+}
+
+var id = 1;
+var email = "test@gmail.com";
+var isActive = true;
+var none;
+
+none = "D";
+none = 5;
+
